0d3b8c76e06a5d6fd9e56294da155195dcc1c220
snowlt23 <shsnow23@gmail.com>
2017-03-10 09:41:56 +0900

    add readdirectorychangesw

--- nimwatch.nim
+++ nimwatch.nim
@@ -3,29 +3,114 @@ import windows
 import asyncdispatch
 
 type
-  Watcher = ref object
-    hNotif: HANDLE
-    path*: string
-    callbacks*: seq[proc ()]
+  Watcher* = ref object
+    target*: string
+    hDir*: HANDLE
+  FileActionKind* = enum
+    actionAdd
+    actionRemove
+    actionModify
+    actionRenameOld
+    actionRenameNew
+  FileAction* = object
+    kind*: FileActionKind
+    filename*: string
 
-proc newWatcher*(path: string): Watcher =
+type
+  FILE_NOTIFY_INFORMATION* = object
+    NextEntryOffset*: DWORD
+    Action*: DWORD
+    FileNameLength*: DWORD
+    FileName*: seq[WCHAR]
+
+const FILE_ACTION_ADDED = 0x00000001
+const FILE_ACTION_REMOVED = 0x00000002
+const FILE_ACTION_MODIFIED = 0x00000003
+const FILE_ACTION_RENAMED_OLD_NAME = 0x00000004
+const FILE_ACTION_RENAMED_NEW_NAME = 0x00000005
+
+converter toWINBOOL(b: bool): WINBOOL = cast[WINBOOL](b)
+converter toBool(b: WINBOOL): bool = cast[bool](b)
+converter toDWORD(x: int): DWORD = cast[DWORD](x)
+
+proc ReadDirectoryChangesW*(
+  hDirectory: HANDLE,
+  lpBuffer: LPVOID, 
+  nBufferLength: DWORD, 
+  bWatchSubtree: WINBOOL, 
+  dwNotifyFilter: DWORD,
+  lpBytesReturned: LPDWORD, 
+  lpOverlapped: LPOVERLAPPED, 
+  lpCompletionRoutine: LPOVERLAPPED_COMPLETION_ROUTINE,
+): WINBOOL {.importc.}
+
+proc newWatcher*(target: string): Watcher =
   new result
-  let filter = FILE_NOTIFY_CHANGE_LAST_WRITE
-  result.hNotif = windows.FindFirstChangeNotification(cast[LPCSTR](path.cstring), cast[WINBOOL](true), cast[DWORD](filter))
-  result.path = path
-  if result.hNotif == INVALID_HANDLE_VALUE:
-    raise newException(IOError, "couldnt start watch in: " & path)
+  result.hDir = CreateFile(
+    cast[LPCSTR](target.cstring), 
+    FILE_LIST_DIRECTORY, 
+    FILE_SHARE_READ or FILE_SHARE_WRITE or FILE_SHARE_DELETE, 
+    cast[LPSECURITY_ATTRIBUTES](nil), 
+    OPEN_EXISTING, 
+    FILE_FLAG_BACKUP_SEMANTICS or FILE_FLAG_OVERLAPPED,
+    cast[HANDLE](nil)
+  )
+  if result.hDir == INVALID_HANDLE_VALUE:
+    raise newException(IOError, "not existing file or directory: " & target)
 
-proc wait*(watcher: Watcher) =
-  discard windows.WaitForSingleObject(watcher.hNotif, INFINITE)
-  discard windows.FindNextChangeNotification(watcher.hNotif)
+proc watch*(watcher: Watcher): Future[seq[FileAction]] {.async.} =
+  var buf: array[10, FILE_NOTIFY_INFORMATION]
+  var pBuf = buf[0].addr
 
-template watch*(path: string, body: untyped) =
-  let watcher = newWatcher(path)
+  let filter =
+    FILE_NOTIFY_CHANGE_FILE_NAME or FILE_NOTIFY_CHANGE_DIR_NAME or FILE_NOTIFY_CHANGE_ATTRIBUTES or FILE_NOTIFY_CHANGE_SIZE or FILE_NOTIFY_CHANGE_LAST_WRITE
+  let hEvent = CreateEvent(cast[LPSECURITY_ATTRIBUTES](nil), true, false, cast[LPCSTR](nil))
+  discard ResetEvent(hEvent)
+  var olp: OVERLAPPED
+  olp.hEvent = hEvent
+
+  if not ReadDirectoryChangesW(watcher.hDir, cast[LPVOID](pBuf), sizeof(FILE_NOTIFY_INFORMATION) * 10, true, filter, cast[LPDWORD](nil), cast[LPOVERLAPPED](olp.addr), cast[LPOVERLAPPED_COMPLETION_ROUTINE](nil)):
+    raise newException(IOError, "couldn't watch directory changes")
   while true:
-    watcher.wait()
-    body
+    let waitResult = WaitForSingleObject(hEvent, 500)
+    if waitResult != WAIT_TIMEOUT:
+      break
+    echo "."
 
-when isMainModule:
-  watch "./":
-    echo "detect file changes!"
+  var retsize: DWORD
+  if not GetOverlappedResult(watcher.hDir, olp, retsize, false):
+    raise newException(IOError, "couldn't get overlapped result")
+
+  var pData = cast[ptr FILE_NOTIFY_INFORMATION](pBuf)
+  result = @[]
+  while true:
+    var action: FileAction
+
+    case pData[].Action
+    of FILE_ACTION_ADDED:
+      action.kind = actionAdd
+    of FILE_ACTION_REMOVED:
+      action.kind = actionRemove
+    of FILE_ACTION_MODIFIED:
+      action.kind = actionModify
+    of FILE_ACTION_RENAMED_OLD_NAME:
+      action.kind = actionRenameOld
+    of FILE_ACTION_RENAMED_NEW_NAME:
+      action.kind = actionRenameNew
+    else:
+      discard
+
+    let lenBytes = pData[].FileNameLength
+    var filename = newWideCString("", lenBytes)
+    for i in 0..<lenBytes:
+      filename[i] = cast[Utf16Char](pData[].FileName[i])
+    action.filename = $filename
+    result.add(action)
+
+    if pData[].NextEntryOffset == 0:
+      break
+    pData = cast[ptr FILE_NOTIFY_INFORMATION](cast[DWORD](pData) + pData[].NextEntryOffset)
+      
+let watcher = newWatcher("./testdir")
+let ret = waitFor watcher.watch()
+echo ret
